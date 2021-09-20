4302a9b802e52d2c38b550aa306b1cfb50cae2a5
Alicja Malkiewicz <alicjamalkiewicz1@gmail.com>
2021-09-20 19:26:17 +0200

    correcting homework

--- homework6.java
+++ homework6.java
@@ -1,6 +1,10 @@
 package pl.test.Webinar6Homework;
 
+import javax.swing.*;
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class homework6 {
     public static void main(String[] args) {
@@ -23,13 +27,54 @@ public class homework6 {
             System.out.println(secondArray[i]);
 
         System.out.println("---EXCERCISE---3---");
-        System.out.println("---a---");
-        int[] Array10 = new int[21];
-        for (int i = -10; i <= 10; i++) {
-            Array10[i] = i + 1;
-            System.out.println(Arrays.toString(Array10));
-            System.out.println(Array10[5]);
+
+        //a.definiowanie tablicy
+        int[] array10 = {1, 10, 4, 3, 7, -5, 0, -1, -3, -7};
+
+        //b.drukowanie tablicy
+        System.out.println(Arrays.toString(array10));
+
+        //c.wyznacz min/max
+        Arrays.sort(array10);
+        System.out.println("min = " + array10[0]);
+        System.out.println("max = " + array10[array10.length - 1]);
+
+        //e.średnia
+        int sum = 0;
+        for (int element : array10)
+            sum = sum + element;
+
+        System.out.println(sum);
+
+        float element = array10.length;
+        System.out.println(element);
+
+        float average = sum / element;
+        System.out.println(average);
+
+        //f.liczba elementów mniejszych od średniej
+        int smallerElements= 0;
+        for (int arrayElements : array10){
+            if (arrayElements < average);
+            smallerElements = smallerElements + 1;
+
+            System.out.println(smallerElements);
+        }
+
+        //g.liczba elementów wiekszych od średniej
+        int biggerElements= 0;
+        for (int arrayElements : array10){
+            if (arrayElements > average);
+            biggerElements = biggerElements + 1;
+
+            System.out.println(biggerElements);
+        }
+
+        //h.tablica drukowana od odtatniego elementu
+        for(int i = array10.length-1; 1>=0; i--){
+            System.out.println(array10[i]);
         }
 
     }
+
 }
